ec2ae57e82b9cd7c3585aecf39388496f8adc374
Muddasir <muhammadmuddasir162@gmail.com>
2025-06-27 23:13:40 +0500

    fix bug

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from extension import db
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
+# db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-from models import User
+from models import User  # Ensure models.py imports only db, not app
+from routes import *     # Import routes after app initialization
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-import routes
 
 if __name__ == '__main__':
     with app.app_context():
